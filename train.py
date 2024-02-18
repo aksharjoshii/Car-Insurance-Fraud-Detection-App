ca1c5df870cb493e92ec78e75d5db0aae4fd19b1
aksharjoshii <joshiflink@gmail.com>
2024-02-18 10:29:26 +0530

    train.py added

---
+++ train.py
@@ -0,0 +1,42 @@
+from pipelines.training_pipeline import model_pipeline
+from utils.utils import EarlyStopping
+import  torch 
+import wandb
+import os 
+
+# device agnostic interface
+data_dir = 'data_dir/car_images'
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# early_stopping = EarlyStopping(patience=5 , min_delta=0.05, restore_best_weights=True )
+# wandb project/run  config
+wandb_proj = {
+    "project": "Insurance_Fraud_Detection",
+    "name": "efnet0_b16_e15_WRsampling",
+} 
+
+config = dict(
+    epochs = 15,
+    classes = 2, 
+    batch_size = 16,
+    lr = 0.001,
+    architecture = 'EfficientNet_B0',
+    #num_workers = os.cpu_count(),
+    sampling = True,  
+    dropout = 0.3
+)
+
+
+if __name__ == '__main__':
+    wandb.login()
+    model_pipeline(proj_config=wandb_proj, hyperparams=config, data_dir=data_dir, device=device)
+
+
+    
+
+
+
+
+
+    
+    
+
