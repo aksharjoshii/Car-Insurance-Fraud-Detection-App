473bd5ed33c6e3ccee761912279f6265a8bf10ef
aksharjoshii <joshiflink@gmail.com>
2024-02-18 23:54:59 +0530

    complete training by running trian.py

--- train.py
+++ train.py
@@ -10,25 +10,29 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 # early_stopping = EarlyStopping(patience=5 , min_delta=0.05, restore_best_weights=True )
 # wandb project/run  config
 wandb_proj = {
-    "project": "Insurance_Fraud_Detection",
-    "name": "efnet0_b16_e15_WRsampling",
+    "project": "Insurance_Fraud_Final",
+    "name": "efnet0_b16_e15_WRsampling_repalce",
 } 
 
 config = dict(
-    epochs = 15,
+    epochs = 10,
     classes = 2, 
     batch_size = 16,
     lr = 0.001,
     architecture = 'EfficientNet_B0',
     #num_workers = os.cpu_count(),
+    replacement = True,
     sampling = True,  
-    dropout = 0.3
+    dropout = 0.3, 
+    best_weights = 'best_weights_v2.pth'
 )
 
 
 if __name__ == '__main__':
     wandb.login()
     model_pipeline(proj_config=wandb_proj, hyperparams=config, data_dir=data_dir, device=device)
+    
+
 
 
     
